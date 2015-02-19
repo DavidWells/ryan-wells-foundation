cb9346ede0bbb6fc0b98703840f55576148d846f
Inbound Now <david@inboundnow.com>
2015-02-19 09:47:53 -0800

    f

--- wp-content/themes/ryan-wells-foundation/footer.php
+++ wp-content/themes/ryan-wells-foundation/footer.php
@@ -43,7 +43,7 @@ global $dd_paypal_url;
 						<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
 					    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
 					    <div style="position: absolute; left: -5000px;"><input type="text" name="b_ce5d31e8b58fb8251cb227c29_aebf7b685b" tabindex="-1" value=""></div>
-					    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+					    <div class="clear"><input type="submit" value="Stay up to date" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
 					    </div>
 					</form>
 					</div>
