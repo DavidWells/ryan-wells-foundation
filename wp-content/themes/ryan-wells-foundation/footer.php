80b135cab9095ab3473c468a0bb498d7019f57b8
Inbound Now <david@inboundnow.com>
2015-02-19 10:22:34 -0800

    f

--- wp-content/themes/ryan-wells-foundation/footer.php
+++ wp-content/themes/ryan-wells-foundation/footer.php
@@ -40,7 +40,7 @@ global $dd_paypal_url;
 					<form action="//ryanwellsfoundation.us1.list-manage.com/subscribe/post?u=ce5d31e8b58fb8251cb227c29&amp;id=aebf7b685b" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
 					    <div id="mc_embed_signup_scroll">
 
-						<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+						<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Enter your email address" required>
 					    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
 					    <div style="position: absolute; left: -5000px;"><input type="text" name="b_ce5d31e8b58fb8251cb227c29_aebf7b685b" tabindex="-1" value=""></div>
 					    <div class="clear"><input type="submit" value="Stay up to date" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
