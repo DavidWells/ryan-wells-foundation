0854829a644001d7d3fb3c961b2a95be36765487
Inbound Now <david@inboundnow.com>
2015-02-19 09:39:00 -0800

    f

--- wp-content/themes/ryan-wells-foundation/footer.php
+++ wp-content/themes/ryan-wells-foundation/footer.php
@@ -30,6 +30,24 @@ global $dd_paypal_url;
 							<a href="<?php echo ot_get_option( $dd_sn . 'footer_banner_button_link' ); ?>" class="dd-button <?php echo ot_get_option( $dd_sn . 'footer_banner_button_color', 'green' ); ?> big"><?php echo ot_get_option( $dd_sn . 'footer_banner_button_text', 'MAKE A DONATION NOW' ); ?></a>
 						<?php endif; ?>
 
+					<div id="mailchimp-form">
+					<!-- Begin MailChimp Signup Form -->
+
+					<div id="mc_embed_signup">
+					<form action="//ryanwellsfoundation.us1.list-manage.com/subscribe/post?u=ce5d31e8b58fb8251cb227c29&amp;id=aebf7b685b" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+					    <div id="mc_embed_signup_scroll">
+						<label for="mce-EMAIL">Subscribe to our mailing list</label>
+						<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+					    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+					    <div style="position: absolute; left: -5000px;"><input type="text" name="b_ce5d31e8b58fb8251cb227c29_aebf7b685b" tabindex="-1" value=""></div>
+					    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+					    </div>
+					</form>
+					</div>
+
+					<!--End mc_embed_signup-->
+					</div>
+
 					</div><!-- #footer-banner-inner -->
 
 				</div><!-- #footer-banner -->
