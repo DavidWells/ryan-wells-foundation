86b600eba27109e8dac1797b4555f09aa541b84d
Inbound Now <david@inboundnow.com>
2015-02-19 09:44:34 -0800

    f

--- wp-content/themes/ryan-wells-foundation/footer.php
+++ wp-content/themes/ryan-wells-foundation/footer.php
@@ -31,6 +31,7 @@ global $dd_paypal_url;
 						<?php endif; ?>
 					<div id="or_mail">
 						OR
+						<span>Subscribe to our mailing list</span>
 					</div>
 					<div id="mailchimp-form">
 					<!-- Begin MailChimp Signup Form -->
@@ -38,7 +39,7 @@ global $dd_paypal_url;
 					<div id="mc_embed_signup">
 					<form action="//ryanwellsfoundation.us1.list-manage.com/subscribe/post?u=ce5d31e8b58fb8251cb227c29&amp;id=aebf7b685b" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
 					    <div id="mc_embed_signup_scroll">
-						<label for="mce-EMAIL">Subscribe to our mailing list</label>
+
 						<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
 					    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
 					    <div style="position: absolute; left: -5000px;"><input type="text" name="b_ce5d31e8b58fb8251cb227c29_aebf7b685b" tabindex="-1" value=""></div>
