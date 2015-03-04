6dfb07e46a7415f847f284f6a9e5d64079b0b90a
Inbound Now <david@inboundnow.com>
2015-03-04 11:45:41 -0800

    add photo

--- wp-content/themes/ryan-wells-foundation/sidebar-staff.php
+++ wp-content/themes/ryan-wells-foundation/sidebar-staff.php
@@ -7,7 +7,22 @@
 	<div id="sidebar" class="one-third column last">
 
 		<div id="sidebar-inner">
+			<?php
+			$id = get_the_ID();
 
+            if ( has_post_thumbnail() ) {
+                    $imgsrc = wp_get_attachment_image_src( get_post_thumbnail_id( $id ), 'member');
+                    $thumb = $imgsrc[0];
+                    $img = '<img src="'.$thumb.'">';
+            } else {
+                 	$img = '';
+            }
+
+
+            ?>
+            <div id="staff-photo">
+				<?php echo $img;?>
+            </div>
 			<?php if ( ! dynamic_sidebar( 'sidebar-staff' ) ) : ?>
 
 				<!-- No widgets -->
