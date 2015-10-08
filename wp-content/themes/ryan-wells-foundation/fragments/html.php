71c16ee1c9d7ac95beeeaf6123511ec7f62bd219
davidwells <david@inboundnow.com>
2015-10-07 18:01:26 -0700

    add  stff

---
+++ wp-content/themes/ryan-wells-foundation/fragments/html.php
@@ -0,0 +1,22 @@
+<?php
+
+	$section_title 	= $layout['content_section_title'];
+	$section_text 	= $layout['content_text'];
+	$section_id 	= (isset($layout['section_id'])) ? $layout['section_id'] : '';
+	$section_id 	= str_replace("#", "", $section_id);
+	$id = preg_replace('/\s+/', '_', $section_title);
+?>
+
+
+<section id="<?php echo $id; ?>" class="section content-section">
+	<div class="shell">
+
+		<?php if ( $section_title ) : ?>
+			<h2><?php echo apply_filters('the_title', $section_title); ?></h2>
+		<?php endif; ?>
+
+		<?php if ( $section_text ) : ?>
+			<div class="content-text"><?php echo $section_text; ?></div>
+		<?php endif; ?>
+	</div>
+</section>
